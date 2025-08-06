e8e89b795402045c2deeebc537780c56d7762f82
david-loves-etoh <antidim19728@163.com>
2025-08-06 13:22:07 +0800

    Delete docs directory

--- docs/javascripts/components/BaseMenuButton.js
+++
@@ -1,9 +0,0 @@
-Vue.component("menu-button", {
-    template: `
-    <button class="menu-btn" @click="$emit('toggle-menu')">
-        <span class="menu-btn-inner top"/>
-        <span class="menu-btn-inner mid"/>
-        <span class="menu-btn-inner bot"/>
-    </button>
-    `
-});
