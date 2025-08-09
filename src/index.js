5eae132c31b7a3be405da87de20a8c3142372dd5
Endless-e308 <antidim19728@163.com>
2025-08-09 11:30:17 +0800

    v0.0.4 - Chapter 3.2 - Singularity

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import { CHAPTER_3 } from "./chapter-3";
 import { CHAPTER_3_1 } from "./chapter-3_1";
+import { CHAPTER_3_2 } from "./chapter-3_2";
 import CategoryGrouped from "./classes/CategoryGrouped";
 import File from "./classes/File";
 
 const saves = [
     CHAPTER_3,
-    CHAPTER_3_1
+    CHAPTER_3_1,
+    CHAPTER_3_2
 ];
 
 const downloadAllSaves = File.downloadAllSaves;
