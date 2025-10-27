a7bb7518475f59fed8412b3b2ff36242f90f65fe
Endless-e308 <antidim19728@163.com>
2025-10-27 12:45:06 +0800

    Chapter 6.1 - Retribution

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { CHAPTER_4 } from "./chapter-4";
 import { CHAPTER_5 } from "./chapter-5";
 import { CHAPTER_5_1 } from "./chapter-5_1";
 import { CHAPTER_6 } from "./chapter-6";
+import { CHAPTER_6_1 } from "./chapter-6_1";
 import CategoryGrouped from "./classes/CategoryGrouped";
 import File from "./classes/File";
 
@@ -15,7 +16,8 @@ const saves = [
     CHAPTER_4,
     CHAPTER_5,
     CHAPTER_5_1,
-    CHAPTER_6
+    CHAPTER_6,
+    CHAPTER_6_1
 ];
 
 const downloadAllSaves = File.downloadAllSaves;
